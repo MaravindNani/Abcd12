d4ddddd22e7c240af56457d6444477e90ac20a24
maaravin <maaravin@LIN24008358.corp.capgemini.com>
2022-03-21 13:29:07 +0530

    This is My First Commit....

---
+++ src/com/Test1/Abcd12.java
@@ -0,0 +1,16 @@
+package com.Test1;
+
+public class Abcd12 {
+
+	public static void main(String[] args) {
+		String name = "Aravind";
+		if(name == "Aravind") {
+			System.out.println("True");
+		}
+		else {
+			System.out.println("False");
+		}
+
+	}
+
+}
